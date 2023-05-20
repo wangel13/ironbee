9f447470494766e6ef36df707dab399c928c402f
Alexey Shillo <cousnapen@gmail.com>
2023-05-20 19:55:33 +0300

    Add projects api

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,3 +1,13 @@
-export default function Projects() {
-  return <div className="bg-white shadow rounded p-4 my-4">Список проектов</div>
+import prisma from "@/lib/prisma";
+
+export default async function Projects() {
+  const projects = await prisma.project.findMany();
+  return (
+    <div className="bg-white shadow rounded p-4 my-4">
+      Список проектов
+      {projects.map((project) => (
+        <div key={project.id}>{project.id}</div>
+      ))}
+    </div>
+  );
 }
