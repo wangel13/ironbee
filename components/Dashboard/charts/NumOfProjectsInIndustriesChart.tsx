6afaea807337d895b0e5b346034918d40df1d22f
Alexey Shillo <cousnapen@gmail.com>
2023-05-27 16:17:22 +0300

    Add colors to charts

--- components/Dashboard/charts/NumOfProjectsInIndustriesChart.tsx
+++ components/Dashboard/charts/NumOfProjectsInIndustriesChart.tsx
@@ -68,7 +68,14 @@ const NumOfProjectsInIndustriesChart = ({
     datasets: [
       {
         data: map(topIndustriesByNumOfProjects, (i) => i.numOfProjects),
-        backgroundColor: "rgba(255, 99, 132, 0.5)",
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.5)",
+          "rgba(54, 162, 235, 0.5)",
+          "rgba(255, 206, 86, 0.5)",
+          "rgba(75, 192, 192, 0.5)",
+          "rgba(153, 102, 255, 0.5)",
+          "rgba(255, 159, 64, 0.5)",
+        ],
       },
     ],
   };
