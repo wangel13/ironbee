5eca018315947df29294251d247ab2c02c01682c
Alexey Shillo <cousnapen@gmail.com>
2023-05-27 16:20:42 +0300

    Small shadow for charts

--- components/Dashboard/Dashboard.tsx
+++ components/Dashboard/Dashboard.tsx
@@ -14,10 +14,10 @@ const Dashboard = ({ statistics }: DashboardProps) => {
   return (
     <div className="mt-4">
       <div className="grid grid-cols-1 gap-4 md:grid-cols-4 lg:grid-cols-6">
-        <div className="bg-white p-4 rounded-lg shadow-md ">
+        <div className="bg-white p-4 rounded-lg shadow-sm ">
           <OneNumChart title="кол-во проектов" num={statistics.numOfProjects} />
         </div>
-        <div className="bg-white p-4 rounded-lg shadow-md">
+        <div className="bg-white p-4 rounded-lg shadow-sm">
           <OneNumChart
             title="кол-во пользователей"
             num={statistics.numOfUsers}
@@ -25,19 +25,19 @@ const Dashboard = ({ statistics }: DashboardProps) => {
         </div>
       </div>
       <div className="grid grid-cols-1 gap-4 lg:grid-cols-2 mt-4">
-        <div className="bg-white p-4 rounded-lg shadow-md">
+        <div className="bg-white p-4 rounded-lg shadow-sm">
           <NumOfProjectsInIndustriesChart
             topIndustriesByNumOfProjects={
               statistics.topIndustriesByNumOfProjects
             }
           />
         </div>
-        <div className="bg-white p-4 rounded-lg shadow-md">
+        <div className="bg-white p-4 rounded-lg shadow-sm">
           <NumOfProjectsAreasChart
             topAreasByNumOfProjects={statistics.topAreasByNumOfProjects}
           />
         </div>
-        <div className="bg-white p-4 rounded-lg shadow-md">
+        <div className="bg-white p-4 rounded-lg shadow-sm">
           <NumOfProjectsLegalFormsChart
             legalFormsByNumOfProjects={statistics.legalFormsByNumOfProjects}
           />
