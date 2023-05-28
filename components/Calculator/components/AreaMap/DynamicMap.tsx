8d496cb0584af5558b0c20b2fef003c60d0aa36b
Alexey Shillo <cousnapen@gmail.com>
2023-05-28 09:38:11 +0300

    Add map placeholder

--- components/Calculator/components/AreaMap/DynamicMap.tsx
+++ components/Calculator/components/AreaMap/DynamicMap.tsx
@@ -1,7 +1,15 @@
+import LoadingIcon from "@/components/LoadingIcon";
 import dynamic from "next/dynamic";
 
-const AreaMap = dynamic(() => import("../AreaMap/AreaMap"), { ssr: false });
+const AreaMap = dynamic(() => import("../AreaMap/AreaMap"), {
+  ssr: false,
+  loading: () => <div>Загрузка...</div>,
+});
 
 export default function DynamicMap(props: any) {
-  return <AreaMap {...props} />;
+  return (
+    <div className={props?.className}>
+      <AreaMap {...props} />
+    </div>
+  );
 }
